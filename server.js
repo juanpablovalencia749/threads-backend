3de1228df3dc7ed2ee9a0cc8a5cd47bbb7d77b3c
Juan Pablo <juanpablovalencia749@gmail.com>
2024-01-14 18:36:56 -0500

    I have created and declared CORS in the project

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ import post from './routes/post.js';
 import dotenv from 'dotenv';
 import connectDB from './database/config.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
 connectDB()
 
 const app = express();
 const PORT = process.env.PORT || 8080
 
-app.use(express.json()); //To parse JSON data in the req.body
+app.use(cors())
+app.use(express.json()) //To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })) // To parse form data in the req.body
 app.use(cookieParser())
 
