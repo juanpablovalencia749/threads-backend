a5843377f7746689ddc1c9db397cd8e86581a845
Juan Pablo <juanpablovalencia749@gmail.com>
2024-01-18 17:48:56 -0500

    I found bug

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ const app = express();
 const PORT = process.env.PORT || 8080
 
 app.use(cors())
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://threads-fumu.onrender.com'); // Reemplaza con la URL de tu aplicación React
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // Otros encabezados CORS y configuraciones pueden ir aquí
-  next();
-});
 app.use(express.json()) //To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })) // To parse form data in the req.body
 app.use(cookieParser())
